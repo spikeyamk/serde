267e9d66d3f6b4dbb0d9c51ef66dfb50cd0abccd
spikeyamk <spikeyamk@github.com>
2024-11-15 18:55:56 +0100

    common pack_holder modified added max_size function recursively unpacking the variadic parameter pack to get the max serialized size

--- include/serde/common.hpp
+++ include/serde/common.hpp
@@ -59,9 +59,32 @@ namespace Serde {
     constexpr bool is_iterable_v = is_iterable<T>::value;
 
     template<typename ... Args>
-    struct pack_holder {
+    class pack_holder {
+    public:
         template <template < typename ... > typename apply_to_T>
         using apply_to = apply_to_T<Args...>;
+    private:
+        template<size_t max_before, typename Last>
+        static constexpr size_t max_size() {
+            if constexpr(get_serialized_size<Last>() > max_before) {
+                return get_serialized_size<Last>();
+            } else {
+                return max_before;
+            }
+        }
+
+        template<size_t max_before, typename First, typename ... Rest>
+        static constexpr size_t max_size() {
+            if constexpr(get_serialized_size<First>() > max_before) {
+                return max_size<get_serialized_size<First>(), Rest...>();
+            } else {
+                return max_size<max_before, Rest...>();
+            }
+        }
+    public:
+        static constexpr size_t max_size() {
+            return max_size<Args...>();
+        }
     };
 
     template<typename T> 
